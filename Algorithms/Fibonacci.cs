cf8e284bb4cdfe923567175f939e00f4678b3124
Dmitry Harnitski <dharnitski@gmail.com>
2017-01-24 16:54:19 -0500

    add memoized  Fibonachi

---
+++ Algorithms/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Algorithms
+{
+    public class Fibonacci
+    {
+        public int Fib(int n)
+        {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+
+    public class FibonacciMemoized
+    {
+        private readonly Int64[] _memo;
+
+        public FibonacciMemoized(int max)
+        {
+            _memo = new Int64[max + 1];
+        }
+
+        public Int64 Fib(int n)
+        {
+            if (n > _memo.Length)
+                throw new ArgumentException();
+
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+
+            if (_memo[n] > 0)
+                return _memo[n];
+            
+            var fib = Fib(n - 1) + Fib(n - 2);
+            _memo[n] = fib;
+
+            return fib;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var fib = new FibonacciMemoized(100);
+            for (int i = 1; i < 93; i++)
+            {
+                Console.WriteLine($"Fibonachi for {i}: {fib.Fib(i)}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
